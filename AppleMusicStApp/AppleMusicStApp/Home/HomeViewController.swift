17702f67da19cb1e64da6d61cb1b487b977357d1
Youngminah <bluebelsmudi@gmail.com>
2021-01-20 03:20:59 +0900

    애플뮤직앱 완성

--- AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
+++ AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
@@ -39,10 +39,34 @@ extension HomeViewController: UICollectionViewDataSource {
     
     // 헤더뷰 어떻게 표시할까?
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        switch kind {
+        
+        switch kind { // kind의 종류는 크게 해더와 푸터가 있음
+        //헤더인 케이스
         case UICollectionView.elementKindSectionHeader:
+            guard let item = trackMangager.todaysTrack else {
+                return UICollectionReusableView()
+            }
+            
+            //해더, footer등등 를 deque할 땐 supplementaryView임.
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TrackCollectionHeaderView", for: indexPath) as? TrackCollectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            
+            header.update(with: item)
+            header.tapHandler = { item -> Void in
+                //Player를 띄운다.
+                
+                let playerStoryboard = UIStoryboard.init(name: "Player", bundle: nil)
+                guard let playerVC = playerStoryboard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController else {
+                    return
+                }
+                playerVC.simplePlayer.replaceCurrentItem(with: item)
+                self.present(playerVC, animated: true, completion: nil)
+                
+            }
+            
             // TODO: 헤더 구성하기
-            return UICollectionReusableView()
+            return header
         default:
             return UICollectionReusableView()
         }
@@ -53,6 +77,13 @@ extension HomeViewController: UICollectionViewDelegate {
     // 클릭했을때 어떻게 할까?
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // TODO: 곡 클릭시 플레이어뷰 띄우기
+        let playerStoryboard = UIStoryboard.init(name: "Player", bundle: nil)
+        guard let playerVC = playerStoryboard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController else {
+            return
+        }
+        let item = trackMangager.tracks[indexPath.item]
+        playerVC.simplePlayer.replaceCurrentItem(with: item)
+        present(playerVC, animated: true, completion: nil)
     }
 }
 
--- AppleMusicStApp/AppleMusicStApp/Style/DefaultStyle.swift
+++ AppleMusicStApp/AppleMusicStApp/Style/DefaultStyle.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
+
 public enum DefaultStyle {
     public enum Colors {
         public static let tint: UIColor = {
             if #available(iOS 13.0, *) {
+                //UIColor 를 반환할 것인데 , traitCollction를 통하여 현재 다크모드인지 아닌지를 알 수 있음.
                 return UIColor { traitCollction in
+                    //다크모드라면 검은색, 다크모드아니라면 흰색으로 출력된다. 
                     if traitCollction.userInterfaceStyle == .dark {
                         return .white
                     } else {
