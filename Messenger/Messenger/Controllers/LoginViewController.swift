c07b44e3f8e882127297946aa4a5baf0f0eddf86
Youngminah <bluebelsmudi@gmail.com>
2021-06-16 22:49:54 +0900

    Add : 메신저 앱에 필요한 기본셋팅

---
+++ Messenger/Messenger/Controllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by meng on 2021/06/16.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
