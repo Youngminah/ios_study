21e973d72e65c88698199ab4752cb34d1a8ef472
Youngminah <bluebelsmudi@gmail.com>
2021-06-17 01:23:37 +0900

    Update : [메신저앱] 로그인 페이지 UI 구성

--- Messenger/Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Messenger/Controllers/ConversationsViewController.swift
@@ -11,7 +11,19 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            //true로 하면 좀더 보여지는 시간이 길어 이전 화면을 볼 수 있는데 false로하면 띄울 화면만 보여짐.
+            present(nav, animated: false)
+        }
     }
 
 
